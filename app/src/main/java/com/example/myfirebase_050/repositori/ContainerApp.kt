6ca941108223ea1b4b009cecf66534b2f9f29e90
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 20:26:52 +0700

    implementasi ContainerApp dan custom Application class untuk inisialisasi dependency injection repository siswa

--- app/src/main/java/com/example/myfirebase_050/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase_050/repositori/ContainerApp.kt
@@ -1,2 +1,21 @@
 package com.example.myfirebase_050
 
+import android.app.Application
+
+interface ContainerApp{
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp: ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
