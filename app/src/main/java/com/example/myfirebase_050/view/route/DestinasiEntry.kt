ab648994150199e7c701c3cace93bd276c1c3108
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 21:41:57 +0700

    mendefinisikan objek DestinasiEntry sebagai rute navigasi statis untuk halaman tambah data siswa

--- app/src/main/java/com/example/myfirebase_050/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/myfirebase_050/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.myfirebase_050.view.route
 
-object DestinasiEntry {
+import com.example.myfirebase_050.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
