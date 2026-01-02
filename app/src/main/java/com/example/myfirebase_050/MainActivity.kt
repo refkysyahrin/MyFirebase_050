ba15d5d29c4f2cd7c5daa6ca0f73ab18843a9e44
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-02 20:22:42 +0700

    setup main activity untuk memanggil datasiswaapp

--- app/src/main/java/com/example/myfirebase_050/MainActivity.kt
+++ app/src/main/java/com/example/myfirebase_050/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myfirebase_050.ui.theme.MyFirebase_050Theme
+import com.example.myfirebase_050.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirebase_050Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyFirebase_050Theme {
-        Greeting("Android")
     }
 }
