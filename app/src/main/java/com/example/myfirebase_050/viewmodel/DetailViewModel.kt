6b8f12c5709fc158dc526f598fc4e9e6411454ef
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-09 14:08:03 +0700

    mendefinisikan sealed interface StatusUIDetail untuk menangani state Loading, Success, dan Error pada halaman detail siswa

--- app/src/main/java/com/example/myfirebase_050/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase_050/viewmodel/DetailViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.myfirebase_050.viewmodel
 
+import com.example.myfirebase_050.Siswa
+
+sealed interface  StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
