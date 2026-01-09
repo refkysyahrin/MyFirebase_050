6456fb268ad1b3a603faa740b53e55dc1a4718df
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-09 14:18:50 +0700

    implementasi fungsi getSatuSiswa pada DetailViewModel untuk mengambil data detail dari repository dan memperbarui UI state

--- app/src/main/java/com/example/myfirebase_050/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase_050/viewmodel/DetailViewModel.kt
@@ -5,9 +5,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myfirebase_050.RepositorySiswa
 import com.example.myfirebase_050.Siswa
 import com.example.myfirebase_050.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface  StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
@@ -22,6 +25,25 @@ RepositorySiswa): ViewModel() {
             ?: error("idSiswa tidak ditemukan di SavedStateHandle")
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatusiswa(idSiswa) )
+            }
+            catch (e: IOException) {
+                StatusUIDetail.Error
+            }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+        }
+    }
 
 
 }
