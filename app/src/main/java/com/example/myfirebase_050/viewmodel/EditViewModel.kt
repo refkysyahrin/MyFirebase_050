4edf5698b33a4bab2769834dbd1f6b0fa5a29449
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-09 14:44:18 +0700

    implementasi EditViewModel untuk inisialisasi form edit dengan mengambil data siswa berdasarkan id dari savedStateHandle

--- app/src/main/java/com/example/myfirebase_050/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase_050/viewmodel/EditViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.myfirebase_050.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase_050.RepositorySiswa
+import com.example.myfirebase_050.UIStateSiswa
+import com.example.myfirebase_050.toUiStateSiswa
+import com.example.myfirebase_050.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private  val  repositorySiswa:
+RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatusiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+}
